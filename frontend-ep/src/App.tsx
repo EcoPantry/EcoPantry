662e036cf33d7a85197deb3bb9ae3667e98b770a
Ting Kai <wheatgrass.mc@gmail.com>
2025-05-22 00:20:48 +0800

    Add frontend base files

---
+++ frontend-ep/src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+  return (
+    <>
+      <div className='text-xl font-dm-sans'><Button>Hello world</Button></div>
+    </>
+  )
+}
+
+export default App
