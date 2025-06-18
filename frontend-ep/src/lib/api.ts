502efd2a680db3863c12e5fe2c865899f6c76030
Nigel Teo <e0694450@u.nus.edu>
2025-06-18 09:55:48 +0800

    update api formatting

--- frontend-ep/src/lib/api.ts
+++ frontend-ep/src/lib/api.ts
@@ -1,35 +1,35 @@
-// export const BASE_API_URL = "http://localhost:3000"
+// export const BASE_API_URL = "http://localhost:3000";
 export const BASE_API_URL = "http://ecopantry-alb-172202900.ap-southeast-1.elb.amazonaws.com/"
 
 export const api = {
   get: async (endpoint: string) => {
-    const res = await fetch(`${BASE_API_URL}${endpoint}`)
-    if (!res.ok) throw new Error("Failed to fetch")
-    return res.json()
+    const res = await fetch(`${BASE_API_URL}${endpoint}`);
+    if (!res.ok) throw new Error("Failed to fetch");
+    return res.json();
   },
   post: async (endpoint: string, data: any) => {
     const res = await fetch(`${BASE_API_URL}${endpoint}`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data),
-    })
-    if (!res.ok) throw new Error("Failed to post")
-    return res.json()
+    });
+    if (!res.ok) throw new Error("Failed to post");
+    return res.json();
   },
-    put: async (endpoint: string, data: any) => {
-        const res = await fetch(`${BASE_API_URL}${endpoint}`, {
-        method: "PUT",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(data),
-        })
-        if (!res.ok) throw new Error("Failed to update")
-        return res.json()
-    },
-    delete: async (endpoint: string) => {
-        const res = await fetch(`${BASE_API_URL}${endpoint}`, {
-        method: "DELETE",
-        })
-        if (!res.ok) throw new Error("Failed to delete")
-        return res.json()
-    }
-}
+  put: async (endpoint: string, data: any) => {
+    const res = await fetch(`${BASE_API_URL}${endpoint}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    });
+    if (!res.ok) throw new Error("Failed to update");
+    return res.json();
+  },
+  delete: async (endpoint: string) => {
+    const res = await fetch(`${BASE_API_URL}${endpoint}`, {
+      method: "DELETE",
+    });
+    if (!res.ok) throw new Error("Failed to delete");
+    return res.json();
+  },
+};
